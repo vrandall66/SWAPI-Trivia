17cdba63a34990fbc89b7a73083b62030618aa0d
Vanessa Randall <vanessalrandall@gmail.com>
2019-10-15 13:59:50 -0600

    Finish testing App component

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -103,6 +103,23 @@ describe("App", () => {
   });
 
   it("should be able to remove a favorite character from state", () => {
+    let updatedCharacter = {
+      name: "Darth Vader",
+      species: "Human",
+      homeworld: {
+        homeName: "Tatooine",
+        homePopulation: "200000"
+      },
+      films: [
+        "The Empire Strikes Back",
+        "Revenge of the Sith",
+        "Return of the Jedi",
+        "A New Hope"
+      ],
+      characterid: 4,
+      favorite: false
+    };
+
     let favorites = [
       {
         name: "R2-D2",
@@ -128,7 +145,7 @@ describe("App", () => {
         species: "Human",
         homeworld: {
           homeName: "Tatooine",
-          homePopulation: 200000
+          homePopulation: "200000"
         },
         films: [
           "The Empire Strikes Back",
@@ -137,78 +154,43 @@ describe("App", () => {
           "A New Hope"
         ],
         characterid: 4,
-        favorite: true
+        favorite: false
       }
     ];
     wrapper.instance().setState({
       favoriteCharacters: favorites
     });
 
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
-    // FINISH TESTING THIS
+    wrapper.instance().addFavoriteCharacter(updatedCharacter);
+
+    expect(wrapper.state()).toEqual({
+      allMovies: [],
+      currentMovie: {},
+      favQuote: "",
+      favoriteCharacters: [
+        {
+          characterid: 3,
+          favorite: true,
+          films: [
+            "The Empire Strikes Back",
+            "Attack of the Clones",
+            "The Phantom Menace",
+            "Revenge of the Sith",
+            "Return of the Jedi",
+            "A New Hope",
+            "The Force Awakens"
+          ],
+          homeworld: { homeName: "Naboo", homePopulation: "4500000000" },
+          name: "R2-D2",
+          species: "Droid"
+        }
+      ],
+      name: "",
+      ranking: "",
+      showFormModal: true,
+      showPlanetModal: false,
+      showUserMenu: false
+    });
   });
 
   it("should update state when handleFormSubmit has been called", () => {
@@ -322,7 +304,7 @@ describe("App", () => {
     });
   });
 
-  it("should be able to update the userMenu state", () => {
+  it("should be able to update the userMenu state to true", () => {
     wrapper.instance().setState({
       showFormModal: false,
       showPlanetModal: false,
@@ -347,8 +329,34 @@ describe("App", () => {
       favoriteCharacters: [],
       allMovies: [],
       currentMovie: {}
-    })
-  })
+    });
+  });
 
-  
+  it("should update user information in state when a user logs out", () => {
+    wrapper.instance().setState({
+      showFormModal: false,
+      showPlanetModal: false,
+      showUserMenu: false,
+      name: "Vanessa",
+      favQuote: "I know",
+      ranking: "Intermediate",
+      favoriteCharacters: [{}, {}, {}, {}],
+      allMovies: [],
+      currentMovie: {}
+    });
+
+    wrapper.instance().userLogoutReset();
+
+    expect(wrapper.state()).toEqual({
+      showFormModal: true,
+      showPlanetModal: false,
+      showUserMenu: false,
+      name: "",
+      favQuote: "",
+      ranking: "",
+      favoriteCharacters: [],
+      allMovies: [],
+      currentMovie: {}
+    });
+  });
 });
