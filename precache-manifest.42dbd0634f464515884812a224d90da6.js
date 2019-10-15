2d349dccd52fe6049b764f137a2df3e100737510
Vanessa Randall <vanessalrandall@gmail.com>
2019-10-15 16:45:05 -0600

    Updates

---
+++ precache-manifest.42dbd0634f464515884812a224d90da6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5687a3f4a68cb8073df2e501237e9ade",
+    "url": "/SWAPI-Trivia/index.html"
+  },
+  {
+    "revision": "03d528c706f41d6c2fcd",
+    "url": "/SWAPI-Trivia/static/css/main.3a481924.chunk.css"
+  },
+  {
+    "revision": "096d5afefe22c7d64d08",
+    "url": "/SWAPI-Trivia/static/js/2.e15772a3.chunk.js"
+  },
+  {
+    "revision": "03d528c706f41d6c2fcd",
+    "url": "/SWAPI-Trivia/static/js/main.919fef3a.chunk.js"
+  },
+  {
+    "revision": "15a8b2cd276b23e45f3c",
+    "url": "/SWAPI-Trivia/static/js/runtime-main.ff849960.js"
+  },
+  {
+    "revision": "4267e5b5e5623ca892763c50266ebbf1",
+    "url": "/SWAPI-Trivia/static/media/BB8Loading.4267e5b5.gif"
+  },
+  {
+    "revision": "ae9a407e119b7d981e37e590b3a2c500",
+    "url": "/SWAPI-Trivia/static/media/ChooseWisely.ae9a407e.png"
+  },
+  {
+    "revision": "005d62cf567744b328831f1aacdba404",
+    "url": "/SWAPI-Trivia/static/media/JediOrderCrest.005d62cf.png"
+  },
+  {
+    "revision": "0f7e8af533dc22cea86813c671f293ee",
+    "url": "/SWAPI-Trivia/static/media/LightsaberCross.0f7e8af5.png"
+  },
+  {
+    "revision": "6b7da538ef4012871025336346b10170",
+    "url": "/SWAPI-Trivia/static/media/SWEpisode1.6b7da538.jpg"
+  },
+  {
+    "revision": "d4411a2a2adc4b63c8bafa7425ae5048",
+    "url": "/SWAPI-Trivia/static/media/SWEpisode2.d4411a2a.jpg"
+  },
+  {
+    "revision": "ca51a8dd4cf6d55885bb017ed8317a6e",
+    "url": "/SWAPI-Trivia/static/media/SWEpisode3.ca51a8dd.jpg"
+  },
+  {
+    "revision": "0fc817bbf2f835334de88d4e1101be39",
+    "url": "/SWAPI-Trivia/static/media/SWEpisode4.0fc817bb.jpg"
+  },
+  {
+    "revision": "df37c5e46b14ce787549434f371df82b",
+    "url": "/SWAPI-Trivia/static/media/SWEpisode5.df37c5e4.jpg"
+  },
+  {
+    "revision": "1568d5fe1747aec02eeec914415a3d8b",
+    "url": "/SWAPI-Trivia/static/media/SWEpisode6.1568d5fe.jpg"
+  },
+  {
+    "revision": "362ca61d769e7d87de2d612da3cb5c19",
+    "url": "/SWAPI-Trivia/static/media/SWEpisode7.362ca61d.jpg"
+  },
+  {
+    "revision": "41cc740c0aebffaee4ae34699434495b",
+    "url": "/SWAPI-Trivia/static/media/activeHamMenu.41cc740c.png"
+  },
+  {
+    "revision": "a8396881eea0392e3b3d8c56bc224e3f",
+    "url": "/SWAPI-Trivia/static/media/favorited-ds.a8396881.svg"
+  },
+  {
+    "revision": "a8bf7fddbd3182e6a029fa6c4caff706",
+    "url": "/SWAPI-Trivia/static/media/inactiveHamMenu.a8bf7fdd.png"
+  },
+  {
+    "revision": "af66ee2d9a48d29d8cb35adfa750ac56",
+    "url": "/SWAPI-Trivia/static/media/main-background.af66ee2d.jpg"
+  },
+  {
+    "revision": "4f7eceea7ee6b0068b7cf720330b9540",
+    "url": "/SWAPI-Trivia/static/media/not-favorite-ds.4f7eceea.svg"
+  }
+]);
