c44cd3e418133d0bffe78e46575028ae4683e3a4
Vanessa Randall <vanessalrandall@gmail.com>
2019-10-15 12:10:53 -0600

    Add tests to App component

--- src/Components/Character/Character.js
+++ src/Components/Character/Character.js
@@ -5,7 +5,6 @@ import favorited from '../../images/favorited-ds.svg'
 
 const Character = ({ characterInfo, updateCharacterFavorite }) => {
   return characterInfo.map( (character) => {
-    console.log(character)
     const { name, species, homeworld, films, characterid, favorite } = character
     const favoriteCharacter = character => {
       let {
