763721c04ff65a19310c8c7602679c6a8e93b93b
Matthew Malone <m.malone@me.com>
2019-10-15 11:46:31 -0600

    Update styling to user profile drop down menu - making buttons into light sabers

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -27,25 +27,36 @@ const UserProfile = ({
         />
       </div>
       {isOpen ? (
-        <div>
-          <Link to='/favorite-characters'>
-            <button type="button" className="UserProfile__button--sign-out"
-            onClick={updateUserMenuState}> {favoriteCharacters.length} Favorite Characters</button>
-          </Link>
+        <div className='drop-down-menu'>
+          <div className='fav-buton-saber'>
+            <Link to='/favorite-characters'>
+              <button type="button" className="UserProfile__fav-characters"
+              onClick={updateUserMenuState}> {favoriteCharacters.length} Favorite Characters</button>
+            </Link>
+            <Link to='/favorite-characters'>
+              <div className='saberButt'></div>
+            </Link>
+          </div>
           <h5>{userFavQuote}</h5>
           <h5>{userRanking}</h5>
-          <Link to="/">
-            <button
-              type="button"
-              className="UserProfile__button--sign-out"
-              onClick={() => {
-                userLogoutReset() 
-                updateUserMenuState()
-              }}
-            >
-              Sign out
-            </button>
-          </Link>
+          <div className='fav-buton-saber'>
+            <Link to='/'>
+              <div className='saberButt sbleft'></div>
+            </Link>
+            <Link to="/">
+              <button
+                type="button"
+                className="UserProfile__button--sign-out"
+                onClick={() => {
+                  userLogoutReset() 
+                  updateUserMenuState()
+                }}
+              >
+                Sign out
+              </button>
+            </Link>
+            
+          </div>
         </div>
       ) : null}
     </div>
