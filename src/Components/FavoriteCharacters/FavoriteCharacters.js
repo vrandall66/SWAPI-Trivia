3205ab07942729343ceebb9f194d80dc6b461428
Matthew Malone <m.malone@me.com>
2019-10-14 19:41:22 -0600

    Add button from favorite characters page back to all movies

--- src/Components/FavoriteCharacters/FavoriteCharacters.js
+++ src/Components/FavoriteCharacters/FavoriteCharacters.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import Character from '../Character/Character'
 import '../Character/Character.css'
 import './FavoriteCharacters.css'
@@ -6,9 +7,18 @@ import './FavoriteCharacters.css'
 const FavoriteCharacters = ({ characterInfo, addFavoriteCharacter}) => {
 
   return (
+    <div className='FavoriteCharacters'>
+      <div className='toMoviesBtn__div'>
+        <Link to="/movies">
+          <button 
+          type='button'
+          className='backToMovies'>Back To All Episodes</button>
+        </Link>
+      </div>
      <section className="CharactersContainer-section">
       <Character characterInfo={characterInfo} updateCharacterFavorite={addFavoriteCharacter} />
      </section>
+    </div>
   )
 }
 
