b17e1c70a4eeb3986af1d52abda577627606c0e2
Vanessa Randall <vanessalrandall@gmail.com>
2019-10-15 15:33:33 -0600

    Test updateCharacterFavorite method in CharactersContainer component

--- src/Components/CharactersContainer/CharactersContainer.test.js
+++ src/Components/CharactersContainer/CharactersContainer.test.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { shallow } from "enzyme";
 import CharactersContainer from "./CharactersContainer";
+import ScrollingText from "../ScrollingText/ScrollingText";
+import Character from "../Character/Character";
 
 describe("CharactersContainer", () => {
-  let wrapper;
+  let wrapper1;
+  let wrapper2;
+  let BB8Loading;
   let episode = {
     characters: [],
     created: "",
@@ -23,7 +27,7 @@ describe("CharactersContainer", () => {
   let addFavoriteCharacter = jest.fn();
 
   beforeEach(() => {
-    wrapper = shallow(
+    wrapper1 = shallow(
       <CharactersContainer
         episode={episode}
         addFavoriteCharacter={addFavoriteCharacter}
@@ -31,7 +35,178 @@ describe("CharactersContainer", () => {
     );
   });
 
-  it("should match the snapshot", () => {
-    expect(wrapper).toMatchSnapshot();
+  wrapper2 = shallow(
+    <div className="CharactersContainer">
+      <ScrollingText episode={episode} />
+      <section className="CharactersContainer-section">
+        {episode.characters.length === 0 ? (
+          <div className="BB8--Parent-Container">
+            <div className="BB8">
+              <div className="BB8--loading">
+                <h1>Loading Characters</h1>
+                <img
+                  src={BB8Loading}
+                  alt="Loading GIF"
+                  className="MoviesContainer-loading"
+                />
+              </div>
+            </div>
+          </div>
+        ) : (
+          <Character
+            characterInfo={this.state.characters}
+            updateCharacterFavorite={this.updateCharacterFavorite}
+          />
+        )}
+      </section>
+    </div>
+  );
+
+  it("should match the snapshot when menu is collapsed", () => {
+    expect(wrapper1).toMatchSnapshot();
+  });
+
+  it("should match the snapshot when menu is open", () => {
+    expect(wrapper2).toMatchSnapshot();
+  });
+
+  it("should update character favorites", () => {
+    const mockState = {
+      episode,
+      characters: [
+        {
+          name: "Yoda",
+          species: "Yoda's species",
+          homeworld: {
+            homeName: "unknown",
+            homePopulation: "unknown"
+          },
+          films: [
+            "The Empire Strikes Back",
+            "Attack of the Clones",
+            "The Phantom Menace",
+            "Revenge of the Sith",
+            "Return of the Jedi"
+          ],
+          characterid: 20,
+          favorite: true
+        },
+        {
+          name: "R2-D2",
+          species: "Droid",
+          homeworld: {
+            homeName: "Naboo",
+            homePopulation: 4500000000
+          },
+          films: [
+            "The Empire Strikes Back",
+            "Attack of the Clones",
+            "The Phantom Menace",
+            "Revenge of the Sith",
+            "Return of the Jedi",
+            "A New Hope",
+            "The Force Awakens"
+          ],
+          characterid: 3,
+          favorite: true
+        },
+        {
+          name: "Leia Organa",
+          species: "Human",
+          homeworld: {
+            homeName: "Alderaan",
+            homePopulation: 2000000000
+          },
+          films: [
+            "The Empire Strikes Back",
+            "Revenge of the Sith",
+            "Return of the Jedi",
+            "A New Hope",
+            "The Force Awakens"
+          ],
+          characterid: 5,
+          favorite: true
+        }
+      ],
+      remainingCharacters: []
+    };
+
+    let newUnFavorite = {
+      characterid: 5,
+      favorite: false,
+      name: "Leia Organa",
+      films: [
+        "The Empire Strikes Back",
+        "Revenge of the Sith",
+        "Return of the Jedi",
+        "A New Hope",
+        "The Force Awakens"
+      ],
+      species: "Human",
+      homeworld: {
+        homeName: "Alderaan",
+        homePopulation: 2000000000
+      }
+    };
+
+    wrapper1.instance().setState(mockState);
+
+    wrapper1.instance().updateCharacterFavorite(newUnFavorite);
+
+    expect(wrapper1.state().characters).toEqual([
+      {
+        characterid: 20,
+        favorite: true,
+        films: [
+          "The Empire Strikes Back",
+          "Attack of the Clones",
+          "The Phantom Menace",
+          "Revenge of the Sith",
+          "Return of the Jedi"
+        ],
+        homeworld: {
+          homeName: "unknown",
+          homePopulation: "unknown"
+        },
+        name: "Yoda",
+        species: "Yoda's species"
+      },
+      {
+        characterid: 3,
+        favorite: true,
+        films: [
+          "The Empire Strikes Back",
+          "Attack of the Clones",
+          "The Phantom Menace",
+          "Revenge of the Sith",
+          "Return of the Jedi",
+          "A New Hope",
+          "The Force Awakens"
+        ],
+        homeworld: {
+          homeName: "Naboo",
+          homePopulation: 4500000000
+        },
+        name: "R2-D2",
+        species: "Droid"
+      },
+      {
+        characterid: 5,
+        favorite: false,
+        films: [
+          "The Empire Strikes Back",
+          "Revenge of the Sith",
+          "Return of the Jedi",
+          "A New Hope",
+          "The Force Awakens"
+        ],
+        homeworld: {
+          homeName: "Alderaan",
+          homePopulation: 2000000000
+        },
+        name: "Leia Organa",
+        species: "Human"
+      }
+    ]);
   });
 });
--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -16,7 +16,12 @@ const UserProfile = ({
   return (
     <div className="UserProfile">
       <div className="UserProfile--userMenu">
-        <h1 className="UserProfile--userName" onClick={() => updateUserMenuState()}>{userName}</h1>
+        <h1
+          className="UserProfile--userName"
+          onClick={() => updateUserMenuState()}
+        >
+          {userName}
+        </h1>
         <input
           type="image"
           alt="User Profile Dropdown Menu Icon"
@@ -26,35 +31,40 @@ const UserProfile = ({
         />
       </div>
       {isOpen ? (
-        <div className='drop-down-menu'>
-          <div className='fav-button-saber'>
-            <Link to='/favorite-characters'>
-              <button type="button" className="UserProfile__fav-characters"
-              onClick={updateUserMenuState}> {favoriteCharacters.length} Favorite Characters</button>
+        <div className="drop-down-menu">
+          <div className="fav-button-saber">
+            <Link to="/favorite-characters">
+              <button
+                type="button"
+                className="UserProfile__fav-characters"
+                onClick={updateUserMenuState}
+              >
+                {" "}
+                {favoriteCharacters.length} Favorite Characters
+              </button>
             </Link>
-            <Link to='/favorite-characters'>
-              <div className='saberButt'></div>
+            <Link to="/favorite-characters">
+              <div className="saberButt"></div>
             </Link>
           </div>
-          <h5 className='user-profile-text'>"{userFavQuote}"</h5>
-          <h5 className='user-profile-text'>Ranking: {userRanking}</h5>
-          <div className='fav-button-saber'>
-            <Link to='/'>
-              <div className='saberButt sbleft'></div>
+          <h5 className="user-profile-text">"{userFavQuote}"</h5>
+          <h5 className="user-profile-text">Ranking: {userRanking}</h5>
+          <div className="fav-button-saber">
+            <Link to="/">
+              <div className="saberButt sbleft"></div>
             </Link>
             <Link to="/">
               <button
                 type="button"
                 className="UserProfile__button--sign-out"
                 onClick={() => {
-                  userLogoutReset() 
-                  updateUserMenuState()
+                  userLogoutReset();
+                  updateUserMenuState();
                 }}
               >
                 Sign out
               </button>
             </Link>
-            
           </div>
         </div>
       ) : null}
